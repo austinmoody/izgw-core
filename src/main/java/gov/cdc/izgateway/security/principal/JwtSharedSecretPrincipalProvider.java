c9ac61ad3249f97f780f44a3da9978726caac4ee
Keith W. Boone <36998841+keithboone@users.noreply.github.com>
2025-10-10 11:05:02 -0400

    IGDD-2052 jwt shared secret should be treated as base64 encoded data (#58)
    * Updates to core to report more about exceptions in logs.
    
    * Add debugging support for errors.
    
    * Added support for reporting path being accessed.
    
    * Fixed extra parameter in String.format.
    
    * Updating calls to support reporting of URL Path on HTTP Errors.
    
    * IGDD-2052 jwtSharedSecret should be treated as Base64 Encoded data
    
    Updated JwtSharedSecretPrincipalProvider to decode secret as Base64 data
    
    * IGDD-2052 jwtSharedSecret should be treated as base64 encoded data

--- src/main/java/gov/cdc/izgateway/security/principal/JwtSharedSecretPrincipalProvider.java
+++ src/main/java/gov/cdc/izgateway/security/principal/JwtSharedSecretPrincipalProvider.java
@@ -9,37 +9,63 @@ import org.springframework.security.oauth2.jwt.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Base64;
+import java.util.ServiceConfigurationError;
 
 import javax.crypto.spec.SecretKeySpec;
 
+/**
+ * Principal provider that validates JWTs using a shared secret.
+ * @author Audacious Inquiry
+ */
 @Slf4j
 @Component
 public class JwtSharedSecretPrincipalProvider extends AbstractJwtPrincipalProvider {
-    @Value("${jwt.shared-secret:}")
     private String sharedSecret;
+    Boolean isValid = null;
 
+    /**
+	 * Constructor 
+	 * @param groupToRoleMapper The group to role mapper
+	 * @param scopeToRoleMapper The scope to role mapper
+	 * @param jwtTokenExtractor The JWT token extractor
+	 * @param sharedSecret The shared secret for validating JWTs (base64-encoded)
+	 */
     @Autowired
     public JwtSharedSecretPrincipalProvider(@Nullable GroupToRoleMapper groupToRoleMapper,
                                             @Nullable ScopeToRoleMapper scopeToRoleMapper,
-                                            JwtTokenExtractor jwtTokenExtractor) {
+                                            JwtTokenExtractor jwtTokenExtractor,     
+                                            @Value("${jwt.shared-secret:}") String sharedSecret
+    ) {
         super(groupToRoleMapper, scopeToRoleMapper, jwtTokenExtractor);
+    	this.sharedSecret = sharedSecret;
     }
 
     @Override
     protected NimbusJwtDecoder createDecoder() {
-        SecretKeySpec secretKey = new SecretKeySpec(sharedSecret.getBytes(), "HmacSHA256");
-        return NimbusJwtDecoder
-                .withSecretKey(secretKey)
-                .build();
+    	try {
+	    	SecretKeySpec secretKey = new SecretKeySpec(Base64.getDecoder().decode(sharedSecret), "HmacSHA256");
+	        return NimbusJwtDecoder
+	                .withSecretKey(secretKey)
+	                .build();
+    	} catch (IllegalArgumentException ex) {
+    		throw new ServiceConfigurationError("Failed to decode JWT shared secret. Ensure it is base64-encoded.", ex);
+    	}
     }
 
     @Override
     protected boolean validConfiguration() {
-        if (StringUtils.isNotBlank(sharedSecret)) {
-            return true;
-        }
-
+    	if (isValid != null) {
+			return isValid;
+		}
+    	try {
+	        if (StringUtils.isNotBlank(sharedSecret) && Base64.getDecoder().decode(sharedSecret) != null) {
+	            return isValid = true;
+	        }
+    	} catch (IllegalArgumentException ex) {
+    		log.warn("Invalid JWT shared secret was set, ensure it is base64 encoded. JWT authentication is disabled.");// Ignore - will log below
+            return isValid = false;
+    	}
         log.warn("No JWT shared secret was set. JWT authentication is disabled.");
-        return false;
+        return isValid = false;
     }
 }
